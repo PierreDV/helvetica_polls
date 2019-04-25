2870dca972420200ae1a45b9acc14521777b74a4
PierreDV <p.verteuil@gmail.com>
2019-04-25 10:47:15 +0200

    update App to fetch poll data from json file

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,17 @@ class App extends Component {
     mockResult: {}
   };
 
+  componentDidMount() {
+    fetch('./swiss-vote-results-sample.json')
+      .then(res => res.json())
+      .then(res => this.setState({ mockResult: res }))
+      .catch(error => console.error("FetchError:", error));
+  }
+
   render() {
     return (
       <div>
-        <ResultsTable {...this.state.mockResult}/>
+        <ResultsTable {...this.state.mockResult[0]}/>
       </div>
     );
   }
